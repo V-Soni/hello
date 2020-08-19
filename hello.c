367507f5a195864b2458ae293706f13fc6bd9b22
nnelson15 <69835724+nnelson15@users.noreply.github.com>
2020-08-19 11:54:49 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nick Nelson
+ *         nnelson15@huskers.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nick Nelson\n");
+
+  return 0;
+}
