c5d1735722981bd9667698df01aca4d776ee27b4
V-Soni <40788535+V-Soni@users.noreply.github.com>
2020-10-01 16:56:18 +0530

    Updated hello.c

--- hello.c
+++ hello.c
@@ -9,12 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-
-  printf("Nick Nelson\n");
-  printf("Undeclared Major\n");
-  printf("Ellen Diep!\n");
-  printf("Computer Science Major\n");
-
+int main() {
+  printf("Nick Nelson\n Undeclared Major\n Ellen Diep!\n Computer Science Major\n");
   return 0;
 }
